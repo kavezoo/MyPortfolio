9b77656df76c809e284c61d37a94d691cc9d905c
KaveZoo <kavezoo@gmail.com>
2026-09-07 19:03:27 +0200

    installed CakeDC / User

--- src/Application.php
+++ src/Application.php
@@ -57,6 +57,9 @@ class Application extends BaseApplication
         );
 
         $this->addPlugin('KvAdmin', ['routes' => true, 'bootstrap' => true]);
+        $this->addPlugin(\CakeDC\Users\Plugin::class);
+        // Uncomment the line below to load your custom users.php config file
+        Configure::write('Users.config', ['users']);
     }
 
     /**
